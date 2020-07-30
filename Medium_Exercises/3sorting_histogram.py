644f49a5bd26ca53dbab7fe4f5bf9dcbf6ebe206
Matthew Everett <matthew@mjheverett.com>
2020-07-30 18:41:39 -0400

    completed medium 3 exercise

---
+++ Medium_Exercises/3sorting_histogram.py
@@ -0,0 +1,18 @@
+input_string = input("Please enter a sentence: ")
+string_dictionary = {}
+input_words = input_string.split()
+
+for i in range(len(input_words)):
+    letter = str(input_words[i])
+    if letter in string_dictionary:
+        string_dictionary[letter] += 1
+    else:
+        string_dictionary[letter] = 1
+
+print(string_dictionary)
+
+sort_dictionary = sorted(string_dictionary.items(), key=lambda x: x[1], reverse=True)
+print("The top three words are:")
+print(sort_dictionary[0])
+print(sort_dictionary[1])
+print(sort_dictionary[2])
