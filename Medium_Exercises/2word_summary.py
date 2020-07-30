0efa1d6326b9495050de94e6be2c51ddd8ac6962
Matthew Everett <matthew@mjheverett.com>
2020-07-30 18:04:44 -0400

    completed medium 2 exercise

--- Medium_Exercises/2word_summary.py
+++ Medium_Exercises/2word_summary.py
@@ -1,9 +1,9 @@
 input_string = input("Please enter a sentence: ")
-
 string_dictionary = {}
+input_words = input_string.split()
 
-for i in range(len(input_string)):
-    letter = str(input_string[i])
+for i in range(len(input_words)):
+    letter = str(input_words[i])
     if letter in string_dictionary:
         string_dictionary[letter] += 1
     else:
