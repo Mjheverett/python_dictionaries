8304f31180d4b68239b8276e8a08e3987243dbc0
Matthew Everett <matthew@mjheverett.com>
2020-07-30 16:17:36 -0400

    completed small exercises

---
+++ Small_Exercises/3friend_counter.py
@@ -0,0 +1,22 @@
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+}
+
+friends = ramit["friends"]
+friends_count = len(friends)
+ramit["friends_count"] = friends_count
+print(ramit)
