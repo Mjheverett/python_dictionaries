3b6570ca5cc03a9609767da594a2535151f609c1
Matthew Everett <matthew@Mac-mini.localdomain>
2020-07-30 16:29:56 -0400

    completed medium 1

---
+++ Medium_Exercises/1letter_summary.py
@@ -0,0 +1,12 @@
+input_string = input("Please enter a word: ")
+
+string_dictionary = {}
+
+for i in range(len(input_string)):
+    letter = str(input_string[i])
+    if letter in string_dictionary:
+        string_dictionary[letter] += 1
+    else:
+        string_dictionary[letter] = 1
+
+print(string_dictionary)
