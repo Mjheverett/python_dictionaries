b07c5209d6045363e3df12014677814d10619723
Matthew Everett <matthew@mjheverett.com>
2020-07-30 16:44:04 -0400

    updated code

---
+++ nested_dictionaries.py
@@ -0,0 +1,12 @@
+digitalcrafts = {
+    'US': {
+        'Georgia': {
+            'Atlanta': '3423 Piedmont Road N# #420'
+        },
+        'Texas': {
+            'Houston': '3302 Canal St.'
+        }
+    }
+}
+
+print(digitalcrafts['US'])
