2b6bde014bbd691e11246a659bceec3ad9a9ad80
Matthew Everett <matthew@mjheverett.com>
2020-07-31 10:53:08 -0400

    started large exercise

--- Large_Exercises/1phonebook_app.py
+++ Large_Exercises/1phonebook_app.py
@@ -1,1 +1,13 @@
-# fix github
+phonebook = {}
+print("Electronic Phone Book")
+print("=====================")
+print("1. Look up an entry")
+print("2. Set an entry")
+print("3. Delete an entry")
+print("4. List all entries")
+print("5. Quit")
+print("")
+selection = input("What do you want to do? ")
+
+
+
