fbcb7f58336bdc8a556e77db863c8e6df8dff6ac
Matthew Everett <matthew@mjheverett.com>
2020-08-02 14:39:25 -0400

    started restaurant exercise

---
+++ Restaurant_receipt_exercise.py
@@ -0,0 +1,18 @@
+menu = {
+    "Brunch" : {
+        "Steak and Eggs": 16.99, 
+        "Three Egg Breakfast": 8.99, 
+        "Egg Benedict": 11.99, 
+        "Biscuit and Gravy": 7.99, 
+        "Chicken Fingers": 10.99, 
+        "Chicken Wrap": 8.99, 
+        "Steak Salad" : 1.99
+    },
+    "Drinks": {
+        "Soft Drink": 1.99, 
+        "Coffee": 1.99, 
+        "Orange Juice": 0.99, 
+        "Milk": 0.55, 
+        "Water": 0.00
+    } 
+}
